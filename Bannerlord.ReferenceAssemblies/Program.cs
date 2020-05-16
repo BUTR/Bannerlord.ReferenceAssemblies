82709eb696ad85e8d84645f0416bf5b1cfaf2bc5
Vitaly Mikhailov <personal@aragas.org>
2020-05-17 02:49:17 +0300

    Fixed references

--- Bannerlord.ReferenceAssemblies/Program.cs
+++ Bannerlord.ReferenceAssemblies/Program.cs
@@ -250,7 +250,7 @@ namespace Bannerlord.ReferenceAssemblies
             foreach (var file in rootFolder.GetFolder("bin").GetFolder("Win64_Shipping_Client").GetModuleFiles(isCore))
             {
                 var args = $"-f|--keep-non-public|-o|{Path.Combine(outputFolder.Path, file.Name)}|{file.Path}".Split('|');
-                ReferenceAssemblyGenerator.CLI.Program.Main(args);
+                ReferenceAssemblyGenerator.Program.Main(args);
             }
         }
 
