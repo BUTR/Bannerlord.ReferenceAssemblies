934445a2d5c0f1b236b0316cbc4034a0039329c8
Tyler Young <tyler.young@impromptu.ninja>
2020-05-17 16:23:29 -0400

    incl exe

--- Bannerlord.ReferenceAssemblies/PathHelpers.cs
+++ Bannerlord.ReferenceAssemblies/PathHelpers.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using PCLExt.FileStorage;
 
 namespace Bannerlord.ReferenceAssemblies
@@ -10,8 +11,8 @@ namespace Bannerlord.ReferenceAssemblies
         public static IFolder GetModuleFolder(this IFolder folder, string module, bool isCore = true)
             => isCore ? folder : folder.CreateFolder("Modules", CreationCollisionOption.OpenIfExists).CreateFolder(module, CreationCollisionOption.OpenIfExists);
 
-        public static IList<IFile> GetModuleFiles(this IFolder folder, bool isCore = true)
-            => isCore ? folder.GetFiles("TaleWorlds.*") : folder.GetFiles();
+        public static IEnumerable<IFile> GetModuleFiles(this IFolder folder, bool isCore = true)
+            => isCore ? folder.GetFiles("TaleWorlds.*.dll").Concat(folder.GetFiles("*.exe")) : folder.GetFiles();
 
     }
 
